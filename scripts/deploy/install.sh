537569ab1c9863ffe6eea96a913e9a75973b573c
simpsosns01 <ilikekobe01@gmail.com>
2023-02-07 14:51:42 +0800

    test

--- scripts/deploy/install.sh
+++ scripts/deploy/install.sh
@@ -25,11 +25,11 @@ if [ -d $NODE_MODULES_PATH ]; then
      git_log=$(get_git_log $START_COMMIT $END_COMMIT)
      grep_package_json=$(echo $git_log | grep package.json)
      if [ -z "$grep_package_json" ]; then
+      echo "package.json is not modified and use codebuild cache"
+     else
       echo "package.json is modified"
       delete_node_modules
       install_node_modules
-     else
-      echo "package.json is not modified and use codebuild cache"
      fi
   else
    echo "node_modules dir exist but empty......"
